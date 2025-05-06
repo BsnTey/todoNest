fd9c99c4562cfeb541b6c2c07e94382d4fe08128
Ruslan Semak <ruslan.semack1501@gmail.com>
2025-05-06 18:37:11 +0300

    feat: Добавлен ESLint + Prettier

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
-import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
 
 const bootstrap = async () => {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
 
-  await app.listen(2000)
-}
+  await app.listen(2000);
+};
 
 bootstrap();
