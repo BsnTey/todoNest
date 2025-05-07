61db7bb561a637eb66843547f5a890a09e4fa47a
BsnTey <kirill2494@mail.ru>
2025-05-07 17:58:33 +0300

    feat: add mo task, user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { UserModule } from './user/user.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
   controllers: [AppController],
+  imports: [UserModule, TaskModule],
 })
 export class AppModule {}
