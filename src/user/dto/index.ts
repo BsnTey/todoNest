ea5c229efaf83e4392b18465f734a2d62485c235
BsnTey <kirill2494@mail.ru>
2025-05-08 16:54:37 +0300

    feat: add user co and s methods

---
+++ src/user/dto/index.ts
@@ -0,0 +1,6 @@
+export * from './changePassword-user.dto';
+export * from './create-user.dto';
+export * from './login-user.dto';
+export * from './logout-user.dto';
+export * from './restorePassword-user.dto';
+export * from './update-user.dto';
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,95 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import {
+  ChangePasswordDto,
+  CreateUserDto,
+  LoginUserDto,
+  LogoutUserDto,
+  RestorePasswordDto,
+  UpdateProfileDto,
+} from './dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('register')
+  async registerUser(@Body() user: CreateUserDto): Promise<CreateUserDto> {
+    return this.userService.registerUser(user);
+  }
+
+  @Post('login')
+  @HttpCode(200)
+  async loginUser(@Body() user: LoginUserDto): Promise<LoginUserDto> {
+    return this.userService.loginUser(user);
+  }
+
+  @Post('logout')
+  @HttpCode(200)
+  async logoutUser(@Body() user: LogoutUserDto): Promise<LogoutUserDto> {
+    return this.userService.logoutUser(user);
+  }
+
+  @Post('refresh')
+  @HttpCode(200)
+  async refreshTokens(@Body() refreshDto: LoginUserDto): Promise<LoginUserDto> {
+    return this.userService.refreshTokens(refreshDto);
+  }
+
+  @Post('password/restore')
+  @HttpCode(200)
+  async restorePassword(
+    @Body() data: RestorePasswordDto,
+  ): Promise<RestorePasswordDto> {
+    return this.userService.restorePassword(data);
+  }
+
+  @Post(':id/block')
+  @HttpCode(200)
+  async blockUser(@Param('id') id: string): Promise<boolean> {
+    return this.userService.blockUser(id);
+  }
+
+  @Post(':id/unblock')
+  @HttpCode(200)
+  async unblockUser(@Param('id') id: string): Promise<boolean> {
+    return this.userService.unblockUser(id);
+  }
+
+  @Get('users')
+  async getAllUsers(): Promise<[]> {
+    return this.userService.getAllUsers();
+  }
+
+  @Get('profile/:id')
+  async getUserProfile(@Param('id') id: string): Promise<string> {
+    return this.userService.getUserProfile(id);
+  }
+
+  @Get('profile/telegram-link')
+  async getTelegramLink(): Promise<string> {
+    return this.userService.getTelegramLink();
+  }
+
+  @Put('profile')
+  async updateProfile(
+    @Body() user: UpdateProfileDto,
+  ): Promise<UpdateProfileDto> {
+    return this.userService.updateProfile(user);
+  }
+
+  @Put('password/change')
+  async changePassword(
+    @Body() data: ChangePasswordDto,
+  ): Promise<ChangePasswordDto> {
+    return this.userService.changePassword(data);
+  }
+}
